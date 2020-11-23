63b52cddf70dd3650b3135cfead3a3cb5b8c796e
Rowan Thorley <rghthorley@gmail.com>
2020-11-24 00:40:37 +1300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let countArr = [0, 0, 0, 0, 0, 0];
+let pArr = [0, 0, 0, 0, 0, 0];
+let peices = 0;
+function peice(val) {
+    peices++;
+    countArr[val]++;
+    for (i = 0; i < countArr.length; i++){
+        pArr[i] = Math.round((countArr[i]/peices)*100);
+        document.getElementById("output" + i).innerHTML = pArr[i] + "% <br> Count: " + countArr[i];
+    }
+}
